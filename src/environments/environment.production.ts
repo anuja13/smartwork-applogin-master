8e307c1d18990b3ea5824396ccd8f38e626de584
anuja13 <anujagit@gmail.com>
2025-04-01 11:02:25 +0200

    chnaged dev env urls

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -2,8 +2,10 @@ export const environment = {
     production: true,
     backend: {
       endpoints:{
-        savePasswordUrl:  "https://smartwork-dev-backend-user.idi.ntnu.no/login/app_credentials",
-        validateTokenUrl: "https://smartwork-dev-backend-user.idi.ntnu.no/login/validate_token"
+        savePasswordUrl:  "https://smartwork-dev-backend-user.idi.ntnu.no/login/app_credentials",  //development server
+        //savePasswordUrl:  "https://smartwork-prod-backend-user.idi.ntnu.no/login/app_credentials", //production server
+        validateTokenUrl: "https://smartwork-dev-backend-user.idi.ntnu.no/login/validate_token"  //development server
+        //validateTokenUrl: "https://smartwork-prod-backend-user.idi.ntnu.no/login/validate_token", //production server
       }
     }
   };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,14 @@ export const environment = {
     production: false,
     backend: {
       endpoints:{
-        savePasswordUrl: "http://localhost:8014/login/app_credentials",
-        validateTokenUrl: 'http://localhost:8014/login/validate_token'
+        //savePasswordUrl: "http://localhost:8014/login/app_credentials",  //for local testing
+        //validateTokenUrl: 'http://localhost:8014/login/validate_token'  //for local testing
+
+        savePasswordUrl:  "https://smartwork-dev-backend-user.idi.ntnu.no/login/app_credentials", //development server
+        //savePasswordUrl:  "https://smartwork-prod-backend-user.idi.ntnu.no/login/app_credentials", //production server
+        
+        validateTokenUrl: "https://smartwork-dev-backend-user.idi.ntnu.no/login/validate_token"   //development server
+        //validateTokenUrl: "https://smartwork-prod-backend-user.idi.ntnu.no/login/validate_token", //production server
       }
     }
   };
