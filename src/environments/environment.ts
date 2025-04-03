c8d2b2ea5a2927fbf71397686b80e416fa2e57b6
anuja13 <anujagit@gmail.com>
2025-04-03 15:42:55 +0200

    dev urls

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,10 +6,10 @@ export const environment = {
         //validateTokenUrl: 'http://localhost:8014/login/validate_token'  //for local testing
 
         savePasswordUrl:  "https://smartwork-dev-backend-limesurvey.idi.ntnu.no/login/app_credentials", //development server
-        //savePasswordUrl:  "https://smartwork-prod-backend-user.idi.ntnu.no/login/app_credentials", //production server
+        //savePasswordUrl:  "https://smartwork-prod-backend-limesurvey.idi.ntnu.no/login/app_credentials", //production server
         
         validateTokenUrl: "https://smartwork-dev-backend-limesurvey.idi.ntnu.no/login/validate_token"   //development server
-        //validateTokenUrl: "https://smartwork-prod-backend-user.idi.ntnu.no/login/validate_token", //production server
+        //validateTokenUrl: "https://smartwork-prod-backend-limesurvey.idi.ntnu.no/login/validate_token", //production server
       }
     }
   };
