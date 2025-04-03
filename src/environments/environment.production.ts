a0c839d8c39fddad44e618f3ab58c27716829a78
anuja13 <anujagit@gmail.com>
2025-04-03 15:41:46 +0200

    dev url correction

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -2,9 +2,9 @@ export const environment = {
     production: true,
     backend: {
       endpoints:{
-        savePasswordUrl:  "https://smartwork-dev-backend-user.idi.ntnu.no/login/app_credentials",  //development server
+        savePasswordUrl:  "https://smartwork-dev-backend-limesurvey.idi.ntnu.no/login/app_credentials",  //development server
         //savePasswordUrl:  "https://smartwork-prod-backend-user.idi.ntnu.no/login/app_credentials", //production server
-        validateTokenUrl: "https://smartwork-dev-backend-user.idi.ntnu.no/login/validate_token"  //development server
+        validateTokenUrl: "https://smartwork-dev-backend-limesurvey.idi.ntnu.no/login/validate_token"  //development server
         //validateTokenUrl: "https://smartwork-prod-backend-user.idi.ntnu.no/login/validate_token", //production server
       }
     }
