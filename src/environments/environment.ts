828e76de4f8c1e499677898c540d77e07c75f838
anuja13 <anujagit@gmail.com>
2025-04-03 14:19:07 +0200

    url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,10 +5,10 @@ export const environment = {
         //savePasswordUrl: "http://localhost:8014/login/app_credentials",  //for local testing
         //validateTokenUrl: 'http://localhost:8014/login/validate_token'  //for local testing
 
-        savePasswordUrl:  "https://smartwork-dev-backend-data.idi.ntnu.no/login/app_credentials", //development server
+        savePasswordUrl:  "https://smartwork-dev-backend-user.idi.ntnu.no/login/app_credentials", //development server
         //savePasswordUrl:  "https://smartwork-prod-backend-user.idi.ntnu.no/login/app_credentials", //production server
         
-        validateTokenUrl: "https://smartwork-dev-backend-data.idi.ntnu.no/login/validate_token"   //development server
+        validateTokenUrl: "https://smartwork-dev-backend-user.idi.ntnu.no/login/validate_token"   //development server
         //validateTokenUrl: "https://smartwork-prod-backend-user.idi.ntnu.no/login/validate_token", //production server
       }
     }
